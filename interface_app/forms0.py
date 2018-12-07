6cae27f5b386e2998fed7a4778b648989d3ebad6
happy_today <>
2018-12-08 07:22:37 +0800

    20181207

--- interface_app/forms0.py
+++ interface_app/forms0.py
@@ -1,6 +1,11 @@
 from django.forms import ModelForm
 from interface_app.models import TestCase
 
+
+"""
+interface_app没有用到form文件，因为数据大都是直接通过接口获取的，没有用到django的form
+跟之前的project_app是不同的思路
+"""
 class TestCaseForm(ModelForm):
     class Meta:
         model = TestCase
